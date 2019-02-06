262108a84ddf8a2848dc9685fcd24af982d4bdc6
Connor Poole <connor.poole@gmail.com>
2019-02-06 22:19:18 +0000

    Update initdb-postgis.sh

--- 10-2.5/initdb-postgis.sh
+++ 10-2.5/initdb-postgis.sh
@@ -12,7 +12,7 @@ UPDATE pg_database SET datistemplate = TRUE WHERE datname = 'template_postgis';
 EOSQL
 
 # Load PostGIS into both template_database and $POSTGRES_DB
-for DB in template_postgis "$POSTGRES_DB"; do
+for DB in template_postgis "$POSTGRES_DB" $(echo $POSTGRES_MULTIPLE_DATABASES | tr ',' ' '); do
 	echo "Loading PostGIS extensions into $DB"
 	"${psql[@]}" --dbname="$DB" <<-'EOSQL'
 		CREATE EXTENSION IF NOT EXISTS postgis;
