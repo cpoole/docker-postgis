632dd86ac7eacf84936e2f2623467d71668ebac5
Matt Lemmone <matthew.lemmone@bird.co>
2019-12-13 17:12:54 -0800

    script updoots

--- 10-2.5/seed_db.sh
+++ 10-2.5/seed_db.sh
@@ -6,31 +6,27 @@ PSQL_TAR_SEED="/opt/postgres-seed.tar.gz"
 
 function exists(){
     if  [[ -z $( stat "$1" 2>/dev/null) ]]; then
-        return 0
+        return 1
     fi
 
-    return 1
+    return 0
 }
 
 function is_empty(){
     local found_files=$( ls "$PSQL_DIR" )
 
     if [ -z "$found_files" ]; then
-        return 0
+        return 1
     fi
 
-    return 1
+    return 0
 }
 
 
-if ! exists "$PSQL_DIR" ; then
-    echo Creating "$PSQL_DIR..."
-    mkdir "$PSQL_DIR"
+if ! exists "$PSQL_TAR_SEED"; then
+    echo "No psql data seed found."
+    exit 1 
 fi
 
-if  is_empty "$PSQL_DIR"; then
-    echo "Planting postgres data seed..."
-    tar -xzf "$PSQL_TAR_SEED" -C "$PSQL_DIR"
-else 
-    echo "psql dir not empty omgggg "
-fi
+echo "Planting psql data seed..."
+tar -xzf "$PSQL_TAR_SEED" -C "$PSQL_DIR"
