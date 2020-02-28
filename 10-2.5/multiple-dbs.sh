14f01692d8fe69bedc94ba8370e3870da1f945ab
Salmaan Rizvi <sar228@cornell.edu>
2020-02-28 01:49:32 -0800

    quote grant too

--- 10-2.5/multiple-dbs.sh
+++ 10-2.5/multiple-dbs.sh
@@ -8,7 +8,7 @@ function create_user_and_database() {
   echo "  Creating database '$database'"
   psql -U $POSTGRES_USER -d postgres  <<-EOSQL
       CREATE DATABASE "$database";
-      GRANT ALL PRIVILEGES ON DATABASE $database TO $POSTGRES_USER;
+      GRANT ALL PRIVILEGES ON DATABASE "$database" TO $POSTGRES_USER;
 EOSQL
 }
 
